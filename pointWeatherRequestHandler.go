cd3a36121446c46c1cdf98cabb197db1bac4a624
AppleCiderGuy <harshalneel@gmail.com>
2022-03-12 16:03:12 -0800

    returning weather forecast for coordinates

--- pointWeatherRequestHandler.go
+++ pointWeatherRequestHandler.go
@@ -8,7 +8,8 @@ import (
 )
 
 type PointWeatherRequestResponse struct {
-	Message string
+	Message  string
+	Forecast Forecast
 }
 
 func PointWeatherRequestHandler(responseWriter http.ResponseWriter, requestPointer *http.Request) error {
@@ -23,23 +24,86 @@ func PointWeatherRequestHandler(responseWriter http.ResponseWriter, requestPoint
 		return fmt.Errorf("request needs query params 'lat' and 'long'")
 	}
 
+	forecast, err := getWeather(lat, long)
+
+	if err != nil {
+		return fmt.Errorf("Internal Failure: unable to fetch weather for providede coordinates(%v,%v) \n -> %s", lat, long, err.Error())
+	}
+
+	json.NewEncoder(responseWriter).Encode(PointWeatherRequestResponse{Message: "-", Forecast: forecast})
+
+	return nil
+}
+
+func getWeather(lat string, long string) (Forecast, error) {
+
 	requestUrl := fmt.Sprintf("https://api.weather.gov/points/%s,%s", lat, long)
 	fmt.Println("requesting weather from url: ", requestUrl)
 	resp, err := http.Get(requestUrl)
 
 	if err != nil {
 		fmt.Println("request error: ", err.Error())
-		return fmt.Errorf("Internal Failure: unable to fetch weather for providede coordinates(%v,%v)", lat, long)
+		return Forecast{}, fmt.Errorf("Internal Failure: unable to fetch weather for providede coordinates(%v,%v)", lat, long)
 	}
 
 	respBody, err := ioutil.ReadAll(resp.Body)
 
-	if err != nil {
-		fmt.Println("parsing error: ", err.Error())
-		return fmt.Errorf("Internal Failure: unable to fetch weather for providede coordinates(%v,%v)", lat, long)
+	var weatherAPIResponse WeatherAPIResponse
+	err1 := json.Unmarshal([]byte(respBody), &weatherAPIResponse)
+
+	if err1 != nil {
+		fmt.Println("Error Parsing response from weather API", err1.Error())
+		return Forecast{}, fmt.Errorf("Unable to parse response")
 	}
 
-	json.NewEncoder(responseWriter).Encode(PointWeatherRequestResponse{Message: fmt.Sprint("weather in RAW form :", string(respBody))})
+	forecast, err := getForecast(weatherAPIResponse.Properties.Forecast)
+	if err != nil {
+		return Forecast{}, err
+	}
 
-	return nil
+	return forecast, nil
+}
+
+func getForecast(url string) (Forecast, error) {
+	fmt.Printf("Fetching Forecast from url %s", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error fetching Forecast from url %s : %s", url, err.Error())
+		return Forecast{}, fmt.Errorf("Error fetching Forast")
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	var forecast ForecastProperties
+	err1 := json.Unmarshal([]byte(respBody), &forecast)
+
+	if err1 != nil {
+		fmt.Printf("Error Parsing Forecast fetched from url: %s , Error: %s", url, err1.Error())
+		return Forecast{}, fmt.Errorf("Error parsing Forast")
+	}
+
+	return forecast.Properties, nil
+}
+
+type WeatherAPIResponse struct {
+	Properties properties `json:"properties"`
+}
+
+type properties struct {
+	Forecast string `json:"forecast"`
+}
+
+type ForecastProperties struct {
+	Properties Forecast `json:"properties"`
+}
+
+type Forecast struct {
+	Periods []PerodicTempratures `json:"periods"`
+}
+
+type PerodicTempratures struct {
+	Name     string `json:"name"`
+	Temp     int    `json:"temperature"`
+	TempUnit string `json:"temperatureUnit"`
+	Short    string `json:"shortForecast"`
+	Details  string `json:"detailedForecast"`
 }
